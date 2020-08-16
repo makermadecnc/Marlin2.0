34289ec881e1885849433f045b4f122ee8b95e96
Austin Williams <austinsk8s2day@gmail.com>
2020-08-16 17:51:04 -0500

    defined proper motherboard

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -128,7 +128,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_MKS_GEN_L
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
